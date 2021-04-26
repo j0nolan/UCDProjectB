5ea92ac45cc172dfe9fbacdf264449bce2683b5c
j0nolan <jnolan@mayococo.ie>
2021-04-26 18:14:53 +0100

    "Categories first Commit"

---
+++ Categories.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+
+## import datset
+dataset = pd.read_csv("Batchelor_Degree_Categories.csv")
+
+##Print first 5 rows
+print(dataset.head())
+
+##print last 5 rows
+print(dataset.tail())
+
+
+
+
